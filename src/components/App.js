1d4e2da4323476c917cf7b513fac132f7182ce20
James Christou <james.christou@gmail.com>
2021-11-09 21:50:51 +1100

    Adding react router to route details and listing requests

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,16 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import TitleComponent from './TitleComponent';
-import SearchComponent from './SearchComponent';
-import SortByComponent from './SortByComponent';
-import SortOrderComponent from './SortOrderComponent';
-import ContactsListComponent from './ContactsListComponent';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListingApp from './listing/App';
+import DetailsApp from './details/App';
 
-export const ConfigContext = React.createContext();
-
-const configValue = {
-  apiBaseUrl: 'https://localhost:44364'
-};
-
-const AppComponent = (props) => {
-  const [isWaitingForResults, setIsWaitingForResults] = useState(true);
-  const [currentListingData, setCurrentListingData] = useState();
-
-  // initialise state. This code will only run when the component mounts
-  useEffect(() => {
-    fetchData("/contacts");
-  }, []);
-
-  const fetchData = async (urlPathAndQuery) => {
-    setIsWaitingForResults(true);
-
-    const result = await axios(
-      `${configValue.apiBaseUrl}${urlPathAndQuery}`,
-    );
-
-    setCurrentListingData(result.data);
-    setIsWaitingForResults(false);
-  };
-
-  if(isWaitingForResults){
-    return (
-      <div>Loading</div>
-    );
-  }
-
+function AppComponent() {
   return (
-    <div className="contacts-listing-page">
-      <ConfigContext.Provider value={configValue} >
-        <TitleComponent titleText={currentListingData.screenTitleText} />
-        {/* <SearchComponent data={currentListingData.searchComponent}/> */}
-        <div className="sorting-container">
-          <SortByComponent sortByData={currentListingData.sortByComponent} fetchData={fetchData}/>
-          <SortOrderComponent sortOrderData={currentListingData.sortOrderComponent} fetchData={fetchData}/>
-        </div>
-        <ContactsListComponent contactsListData={currentListingData.contactsListComponent}/>
-      </ConfigContext.Provider>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ListingApp/>} />
+          <Route path="/details" element={<DetailsApp/>} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/details/App.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+
+const AppComponent = (props) => {
+  return (
+    <div className="contact-details-page">
+      Contact Details
+    </div>
+  );
+}
+
+export default AppComponent;
